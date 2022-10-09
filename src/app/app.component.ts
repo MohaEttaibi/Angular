b3dc134d5179e3999e5a2c4e431a57ff6e585bec
MohaEttaibi <mohammed.ettayby@gmail.com>
2022-10-09 12:16:51 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-app';
+  courses: any[] = [];
+
+  //Instansiate DB from Config
+  constructor(private db: AngularFireDatabase) {
+    this.db.list('/courses').valueChanges()
+    .subscribe(courses => {
+      this.courses = courses;
+      console.log(courses);
+    });
+
+    // this.author = this.db.object('/authors/1').valueChanges();
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAesYK2oqnTnKwU__FdtzFdRSm6azrgAAU",
+    authDomain: "angular-2cd29.firebaseapp.com",
+    databaseURL: "https://angular-2cd29-default-rtdb.firebaseio.com",
+    projectId: "angular-2cd29",
+    storageBucket: "angular-2cd29.appspot.com",
+    messagingSenderId: "288564117987",
+    appId: "1:288564117987:web:a247ec59d4ea1d6783a0d6"
+  }
 };
 
 /*
