2b634085c10aef5a15e61bafd3e8cb800c4bc9d5
MohaEttaibi <mohammed.ettayby@gmail.com>
2022-10-10 15:45:39 +0100

    Part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +6,5 @@ import { AngularFireDatabase } from '@angular/fire/compat/database';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-app';
-  courses: any[] = [];
-
-  //Instansiate DB from Config
-  constructor(private db: AngularFireDatabase) {
-    this.db.list('/courses').valueChanges()
-    .subscribe(courses => {
-      this.courses = courses;
-      console.log(courses);
-    });
-
-    // this.author = this.db.object('/authors/1').valueChanges();
-  }
+  title = "Angular App";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,29 @@
-import { NgModule } from '@angular/core';
+import { AuthorsService } from './authors.service';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {AngularFireModule} from '@angular/fire/compat';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { environment } from 'src/environments/environment';
+import { NgModule } from '@angular/core';
+import { AuthorsComponent } from './authors/authors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorsComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     NgbModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [
+    AuthorsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
